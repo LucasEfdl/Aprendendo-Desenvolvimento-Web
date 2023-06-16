3837b5f6a18a3fb958ed537dc7ae3e911968fa85
LucasEfdl <lucasefdl@gmail.com>
2023-06-16 09:53:58 -0300

    feat: add function weightedAverage

--- Javascript-Moderno-III/14-Estatistica-na-Programação/index.js
+++ Javascript-Moderno-III/14-Estatistica-na-Programação/index.js
@@ -4,3 +4,23 @@ const average = (...numbers) => {
 };
 
 console.log(`A média Aritmética Simples é: ${average(2, 6, 3, 7, 4)}`);
+
+const weightedAverage = (...input) => {
+  const sum = input.reduce(
+    (acum, { not, weight }) => acum + not * (weight ?? 1),
+    0
+  );
+  const weightSum = input.reduce(
+    (acum, input) => acum + (input.weight ?? 1),
+    0
+  );
+  return sum / weightSum;
+};
+
+console.log(
+  `A Média Aritimética Ponderada é: ${weightedAverage(
+    { not: 7, weight: 2 },
+    { not: 9, weight: 5 },
+    { not: 3, weight: 1 }
+  )}`
+);
